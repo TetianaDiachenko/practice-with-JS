ef7f937a11e2a1c6c034dfc9d9ba91fe3f3150c5
TetianaDiachenko <tetianamyhun@gmail.com>
2024-03-07 17:06:33 +0200

    ex.2 Data types

--- scripts.js
+++ scripts.js
@@ -1,4 +1,16 @@
-// Variables
-let userName = 'John';
-let userNumber = 25;
-userNumber = 24;
+// ===========Variables========================================================
+// let userName = 'John';
+// let userNumber = 25;
+// userNumber = 24;
+
+// ===========Data types========================================================
+const storeName = 'Flowers';
+const storeDescription = {
+    'budget': 10000,
+    'employees': ['Ben', 'Anna', 'Kate'],
+    'products': {
+        'roses': 100,
+        'tulips': 60
+    },
+    'open': true
+}
