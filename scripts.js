b1ef048508b8b561369a2c4fbdf16894a453dd6a
TetianaDiachenko <tetianamyhun@gmail.com>
2024-03-08 14:00:05 +0200

    Interaction with the user

--- scripts.js
+++ scripts.js
@@ -3,14 +3,31 @@
 // let userNumber = 25;
 // userNumber = 24;
 
-// ===========Data types========================================================
-const storeName = 'Flowers';
-const storeDescription = {
-    'budget': 10000,
-    'employees': ['Ben', 'Anna', 'Kate'],
-    'products': {
-        'roses': 100,
-        'tulips': 60
-    },
-    'open': true
-}
+// ===========Data types=======================================================
+// const storeName = 'Flowers';
+// const storeDescription = {
+//     'budget': 10000,
+//     'employees': ['Ben', 'Anna', 'Kate'],
+//     'products': {
+//         'roses': 100,
+//         'tulips': 60
+//     },
+//     'open': true
+// }
+
+// ===========Interaction with the user==========================================
+ alert('hi');
+
+const result = confirm("Are you here?");
+console.log(result);
+
+const answer = prompt("Are you 18 already?", "18");
+console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your last name?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers); // Old command, which change everything on the page
+
