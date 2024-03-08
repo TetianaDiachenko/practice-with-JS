4f791c78493494733e07acf6f74e7ffcddebf718
TetianaDiachenko <tetianamyhun@gmail.com>
2024-03-08 15:36:10 +0200

    conditions if/else, ternary operator, switch

--- scripts.js
+++ scripts.js
@@ -16,13 +16,13 @@
 // }
 
 // ===========Interaction with the user==========================================
- alert('hi');
+//  alert('hi');
 
-const result = confirm("Are you here?");
-console.log(result);
+// const result = confirm("Are you here?");
+// console.log(result);
 
-const answer = prompt("Are you 18 already?", "18");
-console.log(answer);
+// const answer = prompt("Are you 18 already?", "18");
+// console.log(answer);
 
 // const answers = [];
 // answers[0] = prompt('What is your name?', '');
@@ -30,4 +30,40 @@ console.log(answer);
 // answers[2] = prompt('How old are you?', '');
 
 // document.write(answers); // Old command, which change everything on the page
+
+// ===========Conditions=========================================================
+
+if (4 == 4) {
+	console.log('Ok!');
+} else {
+	console.log('Error');
+}
+
+const num = 50;
+if(num < 49) {
+	console.log('Error');
+} else if (num > 100) {
+	console.log('Much');
+} else {
+	console.log('Ok!');
+}
+
+//ternary operator: (Condition) ? true : false,
+(num === 50) ? console.log('ok!') : console.log('Error');
+
+// Switch for many conditions
+switch (num) {
+	case 49: 
+		console.log('False');
+		break;
+	case 100:
+		console.log('False');
+		break;
+	case 51:
+		console.log('TRUE');
+		break;
+	default:
+		console.log('Not this time');
+		break;	
+}
 
