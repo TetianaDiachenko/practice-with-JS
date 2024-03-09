d5a60cde3d022dcc315d637d7625f533feb33f03
TetianaDiachenko <tetianamyhun@gmail.com>
2024-03-09 21:04:20 +0200

    Logical operators

--- scripts.js
+++ scripts.js
@@ -33,37 +33,130 @@
 
 // ===========Conditions=========================================================
 
-if (4 == 4) {
-	console.log('Ok!');
-} else {
-	console.log('Error');
-}
+// if (4 == 4) {
+// 	console.log('Ok!');
+// } else {
+// 	console.log('Error');
+// }
 
-const num = 50;
-if(num < 49) {
-	console.log('Error');
-} else if (num > 100) {
-	console.log('Much');
-} else {
-	console.log('Ok!');
-}
+// const num = 50;
+// if(num < 49) {
+// 	console.log('Error');
+// } else if (num > 100) {
+// 	console.log('Much');
+// } else {
+// 	console.log('Ok!');
+// }
 
 //ternary operator: (Condition) ? true : false,
-(num === 50) ? console.log('ok!') : console.log('Error');
+// (num === 50) ? console.log('ok!') : console.log('Error');
 
 // Switch for many conditions
-switch (num) {
-	case 49: 
-		console.log('False');
-		break;
-	case 100:
-		console.log('False');
-		break;
-	case 51:
-		console.log('TRUE');
-		break;
-	default:
-		console.log('Not this time');
-		break;	
+// switch (num) {
+// 	case 49: 
+// 		console.log('False');
+// 		break;
+// 	case 100:
+// 		console.log('False');
+// 		break;
+// 	case 51:
+// 		console.log('TRUE');
+// 		break;
+// 	default:
+// 		console.log('Not this time');
+// 		break;	
+// }
+
+// ===========Logical operators=========================================================
+// const hamburge = 5;
+// const fries = null;
+// if (hamburge && fries) {
+// 	console.log('I am happy');	
+// }
+// console.log((hamburge && fries)); // null
+
+// const hamburge = 2;
+// const fries = 1;
+// const cola = 0;
+// if (hamburge === 3 && cola === 1 && fries) {
+// 	console.log('We are happy!');	
+// } else {
+// 	console.log('We are leaving');
+	
+// }
+
+// const hamburge = 3;
+// const fries = 1;
+// const cola = 0;
+// console.log((hamburge === 3 && cola && fries)); // 0
+// if (hamburge === 3 && cola === 1 && fries) {
+// 	console.log('We are happy!');	
+// } else {
+// 	console.log('We are leaving');
+// }
+
+// console.log(1 && 0); // 0
+// console.log(1 && 5); // 5
+// console.log(null && 5); // null
+// console.log(0 && 'sdfghfewawsedf'); // 0
+
+// const hamburge = 3;
+// const fries = 0;
+// const cola = 0;
+// if (hamburge || cola || fries) { // true
+// 	console.log('We are ok!');	
+// } else {
+// 	console.log('We are leaving');
+// }
+// console.log((hamburge || cola || fries)); // 3
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+// console.log(johnReport || alexReport || samReport || mariaReport); // done
+
+// const hamburge = 3;
+// const fries = 3;
+// const cola = 0;
+// nuggets = 2;
+
+// if (hamburge === 3 && cola === 2 || fries === 3 && nuggets) { // true
+// 	console.log('We are ok!');	
+// } else {
+// 	console.log('We are leaving'); // We are ok
+// }
+// console.log((hamburge === 3 && cola === 2 || fries === 3 && nuggets)); // 2
+
+// tasks for logical operators
+console.log( NaN || 2 || undefined );              // 2
+console.log( NaN && 2 && undefined );              // NaN
+console.log( 1 && 2 && 3 );                        //3
+console.log( !1 && 2 || !3 );                      // false
+console.log( 25 || null && !3 );                   //25
+console.log( NaN || null || !3 || undefined || 5); //5
+console.log( NaN || null && !3 && undefined || 5); //5
+console.log( 5 === 5 && 3 > 1 || 5);               // true
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+if (hamburger === 3 && cola || fries === 3 && nuggets) {   // Done!
+   console.log('Done!')
 }
 
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+if (hamburger || cola || fries === 3 || nuggets) { // Done!
+   console.log('Done!') 
+}
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+if (hamburger && cola || fries === 3 && nuggets) { // Nothing in the console
+   console.log('Done!')
+}
