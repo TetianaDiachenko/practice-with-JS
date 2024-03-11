d29379d3e4235b8f884498bb178308511efdbddf
TetianaDiachenko <tetianamyhun@gmail.com>
2024-03-11 19:27:53 +0200

    tasks for cycles

--- scripts.js
+++ scripts.js
@@ -126,37 +126,172 @@
 // console.log((hamburge === 3 && cola === 2 || fries === 3 && nuggets)); // 2
 
 // tasks for logical operators
-console.log( NaN || 2 || undefined );              // 2
-console.log( NaN && 2 && undefined );              // NaN
-console.log( 1 && 2 && 3 );                        //3
-console.log( !1 && 2 || !3 );                      // false
-console.log( 25 || null && !3 );                   //25
-console.log( NaN || null || !3 || undefined || 5); //5
-console.log( NaN || null && !3 && undefined || 5); //5
-console.log( 5 === 5 && 3 > 1 || 5);               // true
+// console.log( NaN || 2 || undefined );              // 2
+// console.log( NaN && 2 && undefined );              // NaN
+// console.log( 1 && 2 && 3 );                        //3
+// console.log( !1 && 2 || !3 );                      // false
+// console.log( 25 || null && !3 );                   //25
+// console.log( NaN || null || !3 || undefined || 5); //5
+// console.log( NaN || null && !3 && undefined || 5); //5
+// console.log( 5 === 5 && 3 > 1 || 5);               // true
 
 
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-if (hamburger === 3 && cola || fries === 3 && nuggets) {   // Done!
-   console.log('Done!')
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {   // Done!
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+// if (hamburger || cola || fries === 3 || nuggets) { // Done!
+//    console.log('Done!') 
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+// if (hamburger && cola || fries === 3 && nuggets) { // Nothing in the console
+//    console.log('Done!')
+// }
+
+// ===========Сycles=========================================================
+
+// function firstTask() {
+// 	for (let i = 5; i < 11; i++) {
+// 			console.log(i)
+// 		}    	 
+// }
+// firstTask();      // 5 6 7 8 9 10
+
+// function secondTask() {
+//     for (let i = 20; i > 9; i--){
+// 		if (i === 13){
+// 			break;
+// 		}
+// 		console.log(i)  
+// 	 }    
+// }
+// secondTask();   // 20 19 18 17 16 15 14
+
+// function thirdTask() {
+//     for (let i = 2; i <= 10; i++){
+// 		if (i % 2 === 0){
+// 			console.log(i);
+// 		}
+// 	 }
+// }
+// thirdTask();             // 2 4 6 8 10
+
+// let i = 2;
+// while (i <=16) {
+// 	if (i % 2 === 0) {
+// 		i++;
+//       continue;
+//     } else {
+//         console.log(i);
+//     }
+// 	 i++;
+// }                          // 3 5 7 9 11 13 15
+
+// function fifthTask() {
+// 	const arrayOfNumbers = [];
+
+// 	for (let i = 5; i <= 10; i++){
+// 		arrayOfNumbers.push(i);
+// 	};
+// 	console.log(arrayOfNumbers);
+// 	return arrayOfNumbers;
+// }
+// fifthTask(); // [ 5, 6, 7, 8, 9, 10 ]
+
+// Or
+// function fifthTask() {
+// 	const arrayOfNumbers = [];
+
+// 	for (let i = 5; i < 11; i++) {
+// 		 arrayOfNumbers[i - 5] = i;         // or!!!
+// 	}
+
+// 	console.log(arrayOfNumbers);
+// 	return arrayOfNumbers;
+// }
+
+// fifthTask()  // [ 5, 6, 7, 8, 9, 10 ]
+
+
+
+// ==================Сycles*
+
+// function firstTask() {
+// 	const arr = [3, 5, 8, 16, 20, 23, 50];
+// 	const result = [];
+
+// 	for (let i = 0; i < arr.length; i++) {
+// 		result[i] = arr[i];
+// 	}
+// 	console.log(result);
+// 	return result;
+// }
+// firstTask();            //  [3, 5, 8, 16, 20, 23, 50]
+
+// function secondTask() {
+// 	const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// 	for (let i = 0; i < data.length; i++){
+// 		if(typeof(data[i]) === 'number') {
+// 			data[i] = data[i] * 2;
+// 		} else if (typeof(data[i]) === 'string') {
+// 			data[i] = `${data[i]} - done`;
+// 		}
+// 	}
+// 	console.log(data);	
+// 	return data;
+// }
+// secondTask();  // [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+
+// function thirdTask() {
+// 	const data = [5, 10, 'Shopping', 20, 'Homework'];
+// 	const result = [];
+
+// 	for (let i = 1; i <= data.length; i++){
+// 		result[i - 1] = data[data.length - i];
+// 	}
+
+// 	console.log(result);
+// 	return result;
+// }
+// thirdTask(); // [ 'Homework', 20, 'Shopping', 10, 5 ]
+
+// ==================Сycles ** Piramid
+//     *
+// 	***
+//   *****
+//  *******
+// *********
+//***********
+
+function createFigure() {
+	const lines = 5;
+	let result = '';
+
+	for (let i = 0; i <= lines; i++) {
+		for(let j = 0; j < lines - i; j++) {
+			result += " ";
+		}
+		for(let j = 0; j < 2 * i + 1; j++) {
+			result += "*";
+		}
+		result +="\n";
+	}
+
+	console.log(result);
+	return result;
 }
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-if (hamburger || cola || fries === 3 || nuggets) { // Done!
-   console.log('Done!') 
-}
-
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
-if (hamburger && cola || fries === 3 && nuggets) { // Nothing in the console
-   console.log('Done!')
-}
+createFigure();
